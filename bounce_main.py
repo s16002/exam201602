a7fd2b1e79da27e58299fd0a445fdc60a26020ba
Shogo Kushi <s16002@std.it-college.ac.jp>
2016-08-18 09:48:44 +0900

    #Game over 画面とPless space画面を追加 #スペースを押したらゲームが始まるプログラムを追加 #見た目だけのブロックの追加

--- bounce_main.py
+++ bounce_main.py
@@ -1,26 +1,32 @@
+# スペースを押せばゲームが始まるプログラムを追加した
+# Gameover画面とpress space画面を表示するプログラムを追加した
+#block
+
 from tkinter import *
 import random
 
 
 class Ball:
-    def __init__(self, canvas, paddle, color):
+    def __init__(self, canvas, paddle, block, color):
         self.canvas = canvas
         self.paddle = paddle
-        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.block = block
+        self.id = canvas.create_oval(0, 284, 15, 299, fill=color)
         self.canvas.move(self.id, 245, 100)
-        self.x = random.choice((-3, -2, -1, 1, 2, 3))
-        self.y = -3
+        self.x = 0
+        self.y = 0
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
         self.hit_bottom = False
+        self.canvas.bind_all('<KeyPress-space>', self.start)
 
     def hit_paddle(self, pos):
         paddle_pos = self.canvas.coords(self.paddle.id)
         if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
             if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
                 return True
-
         return False
+
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -39,14 +45,19 @@ class Ball:
             self.x = abs(self.x) * -1
 
         if self.hit_paddle(pos):
-            self.y = abs(self.x) * -1
+            self.y = abs(self.y) * -1
+
+    def start(self, event):
+        if self.y == 0:
+            self.x = random.choice((-3, -2, -1, 1, 2, 3))
+            self.y = -4
 
 
 class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
-        self.canvas.move(self.id, 200, 300)
+        self.canvas.move(self.id, 200, 400)
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
         self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
@@ -62,33 +73,52 @@ class Paddle:
             self.x = 0
 
     def turn_left(self, event):
-        self.x = -2
+        self.x = -3
 
     def turn_right(self, event):
-        self.x = 2
+        self.x = 3
+
+
+class Block:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(100, 10, 150, 25, fill=color)
+
+
+class Gameover:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.label = Label(text="Game Over", font=(100)).pack()
 
 
 tk = Tk()
 tk.title("Game")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
-c = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
-c.pack()
+canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
+canvas.pack()
 tk.update()
 
-p = Paddle(c, 'blue')
-ball = Ball(c, p, 'red')
+game_over_text = canvas.create_text(250, 200, text='GAME OVER!', state='hidden',font=("Purisa",50))
+game_start_text = canvas.create_text(250,200, text='Press space!', state='hidden',font=("purisa",50))
+paddle = Paddle(canvas, 'blue')
+block = Block(canvas, 'black')
+ball = Ball(canvas, paddle, block, 'red')
 
 
 def update():
-    if not ball.hit_bottom:
+    if  ball.hit_bottom == False:
+        if ball.x == 0:
+            canvas.itemconfig(game_start_text, state='normal')
+        else:
+            canvas.itemconfig(game_start_text, state='hidden')
         ball.draw()
-        p.draw()
+        paddle.draw()
+        tk.update_idletasks()
+        tk.update()
+        tk.after(10, update)
 
-    tk.update_idletasks()
-    tk.update()
-    tk.after(10, update)
-
-
+    else:
+        canvas.itemconfig(game_over_text, state='normal')
 tk.after(10, update)
 tk.mainloop()
--- exam/exam.py
+++ exam/exam.py
@@ -1,19 +1,84 @@
 # 'hello, {name}!'と出力してください 。
 def hello(name):
-    pass
+    print('hello, {0}!'.format(name))
 
 
 # sentence の文字数を出力してください
 def length(sentence):
-    pass
+    print(len(sentence))
 
 
 # sentence の2文字目から5文字目まで(5文字目は含まない)を出力してください
 def slicing2to5(sentence):
-    pass
+    print(sentence[2:5])
 
 
 # number の符号を出力してください。ただし、0は'0'と出力してください
 def number_sign(number):
-    pass
+    print(('{0}' if number <= 0 else '{1}').format('0' if number == 0 else '-', '+'))
 
+
+# number が素数なら'ok',そうでないなら'ng'と出力してください
+def prime_number(number):
+    count = 0
+    for i in range(1, number):
+        if number % i == 0:
+            count += 1
+    print('{0}'.format('ok' if count <= 1 else 'ng'))
+
+
+# 1からnumberまでの合計を出力してください
+def sum_from_1_to(number):
+    print(sum([i for i in range(1, number + 1)]))
+
+
+# numberの階乗(factorial)を出力してください
+def factorial(number):
+    answer = 1
+    for i in range(1, number+1):
+        answer *= i
+    print(answer)
+
+
+# リストdataの各要素(整数)を3乗した結果をリスト型として返してください
+def cubic_list(data):
+    return [(data[i] ** 3) for i in range(len(data))]
+
+
+# 底辺x,高さyの直角三角形(right angled triangle)の残り1つの辺の長さを返してください
+def calc_hypotenuse(x, y):
+    import math
+    return math.sqrt(x ** 2 + y ** 2)
+
+
+# 底辺x,斜辺vの直角三角形の残り1つの辺の長さを返してください
+def calc_subtense(x, v):
+    import math
+    return math.sqrt(abs(x**2 - v **2))
+
+
+# 三辺の長さがそれぞれx,y,zの三角形の面積を返してください
+def calc_area_triangle(x, y, z):
+    import math
+    return (z * (math.sqrt(x ** 2 - ((x ** 2 - y ** 2 + z ** 2) / (2 * z)) ** 2))) / 2
+
+
+# 引数a,b,cを小数点以下2桁表示で空白切りで表示してください
+def point_two_digits(a, b, c):
+    print('{0} {1} {2}'.format(format(a, '.2f'), format(b, '.2f'), format(c, '.2f')))
+
+
+# リストdataの内容を小さい順でソートした結果を返してください
+def list_sort(data):
+    return sorted(data)
+
+
+# 文字列の並びを逆にしたものを返してください
+def reverse_string(sentence):
+    return sentence[len(sentence)-1::-1]
+
+
+# dateから2016年4月1日までの日数を返してください
+def days_from_date(point):
+    import datetime
+    return abs(int((str((point) - (datetime.date(2016, 4, 1))).split())[0]))
