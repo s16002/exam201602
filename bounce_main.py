ad96985a1b437dae1ca9f8803eab0d1418d4ce58
Shogo Kushi <s16002@std.it-college.ac.jp>
2016-08-19 23:42:39 +0900

    ゲームスタート画面追加 ゲームオーバー画面追加 ゲームをスペースで始めるシステム追加 ブロックの追加 ブロックの全面の当たり判定追加 ボールがブロックに接触時、消滅プログラム追加

--- bounce_main.py
+++ bounce_main.py
@@ -31,25 +31,31 @@ class Ball:
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         block_pos = self.canvas.coords(self.block.id)
-        if pos[1]<=block_pos[3] and pos[1] > block_pos[1]:
-            if pos[0]< block_pos[2]-3 and pos[0] > block_pos[0]+3:
-                if self.y / abs(self.y) == -1:
-                    self.y = abs(self.y)
-        if pos[3]>=block_pos[1] and pos[3] < block_pos[3]:
-            if pos[0]<block_pos[2]-3 and pos[0] > block_pos[0]+3:
-                if self.y/ abs(self.y) == 1:
-                    self.y = abs(self.y) * -1
+        if block_pos:
+            if pos[1]<=block_pos[3] and pos[1] > block_pos[1]:
+                if pos[0]< block_pos[2]-3 and pos[0] > block_pos[0]+3:
+                    if self.y / abs(self.y) == -1:
+                        self.y = abs(self.y)
+                        self.canvas.delete(self.block.id)
 
-        if pos[0] <= block_pos[2] and pos[0] > block_pos[0]:
-            if pos[1] < block_pos[3]-2 and pos[1] > block_pos[1]+2:
-                if self.x /abs(self.x)==-1:
-                    self.x = abs(self.x)
+            if pos[3]>=block_pos[1] and pos[3] < block_pos[3]:
+                if pos[0]<block_pos[2]-3 and pos[0] > block_pos[0]+3:
+                    if self.y / abs(self.y) == 1:
+                        self.y = abs(self.y) * -1
+                        self.canvas.delete(self.block.id)
 
-        if pos[2] >= block_pos[0] and pos[2] < block_pos[2]:
-            if pos[1] < block_pos[3]-2 and pos[1] > block_pos[1]+2:
-                if self.x /abs(self.x) == 1:
-                    self.x = abs(self.x) * -1
-
+            if pos[0] <= block_pos[2] and pos[0] > block_pos[0]:
+                if pos[1] < block_pos[3]-2 and pos[1] > block_pos[1]+2:
+                    if self.x / abs(self.x) == -1:
+                        self.x = abs(self.x)
+                        self.canvas.delete(self.block.id)
+            if pos[2] >= block_pos[0] and pos[2] < block_pos[2]:
+                if pos[1] < block_pos[3]-2 and pos[1] > block_pos[1]+2:
+                    if self.x / abs(self.x) == 1:
+                        self.x = abs(self.x) * -1
+                        self.canvas.delete(self.block.id)
+        else:
+            pass
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -79,7 +85,7 @@ class Ball:
 class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
-        self.id = canvas.create_rectangle(0, 0, 500, 10, fill=color)
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
         self.canvas.move(self.id, 200, 400)
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
@@ -103,17 +109,17 @@ class Paddle:
 
 
 class Block:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, color,  margin_left, margin_top):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 50, fill=color)
-        self.canvas.move(self.id, 10, 10)
+        self.canvas.move(self.id, margin_left, margin_top)
+
 
 
 class Gameover:
     def __init__(self, canvas):
         self.canvas = canvas
         self.label = Label(text="Game Over", font=(100)).pack()
-
 
 tk = Tk()
 tk.title("Game")
@@ -125,9 +131,11 @@ tk.update()
 
 game_over_text = canvas.create_text(250, 200, text='GAME OVER!', state='hidden',font=("Purisa",50))
 game_start_text = canvas.create_text(250,200, text='Press space!', state='hidden',font=("purisa",50))
-
+margin_left=200
+margin_top=80
 paddle = Paddle(canvas, 'blue')
-block = Block(canvas, 'black')
+#block=[Block(canvas,'black', margin )for margin in range(10,400,110)]
+block=Block(canvas,'black',margin_left,margin_top)
 ball = Ball(canvas, paddle, block, 'red')
 
 
